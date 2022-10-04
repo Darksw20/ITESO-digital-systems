f0f64b0e34f22bebf6718c3b4342312b1d694dea
NAVARRO VIÑA <is728671@iteso.mx>
2022-10-04 08:56:05 -0500

    added fullader needs code of display7

---
+++ adder_subs/adder_subs.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04.10.2022 08:41:39
+// Design Name: 
+// Module Name: adder_subs
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module adder_subs(
+    input [3:0] a,
+    input [3:0] b,
+    input sum_rest,
+    input clk,
+    output [6:0] segm,
+    output [3:0] transistor
+    );
+endmodule
---
+++ adder_subs/mux_in_time_4x4.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04.10.2022 08:44:29
+// Design Name: 
+// Module Name: mux_in_time_4x4
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mux_in_time_4x4(
+    input [4:0] dato0,
+    input [4:0] dato1,
+    input [4:0] dato2,
+    input [4:0] dato3,
+    input clk,
+    output [3:0] segm,
+    output [3:0] transistor
+    );
+endmodule
